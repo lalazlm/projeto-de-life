121445e4c8847cafaf4febdc1903337fb79dedc1
lalazlm <laura.zelma.alves@escola.pr.gov.br>
2026-05-11 09:23:23 -0300

    salvar

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
 const botoes = document.querySelectorAll(".botao");
+const botoes = document.querySelectorAll(".aba-conteudo");
 
 for(let i=0;i <botoes.length;i++){
     botoes[i].onclick = function(){
         
         for(let j=0;j<botoes.length;j++){
             botoes[j].classList.remove("ativo");
-        }
-        
-        botoes[i].classList.add("ativo");
+            botoes[i].classList.add("ativo");
     }
     console.log();
-}const contadores = document.querySelectorAll(".contador");
+}
+
+
+const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2026-10-05T00:00:00");
 let tempoAtual = new Date();
 
